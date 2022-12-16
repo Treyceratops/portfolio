87151f9b22ccad9ee5147f9afbdf7c4e37fcd266
Treyceratops <treytowers@yahoo.com>
2022-12-15 17:41:49 -0800

    add github link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import SideProfile from '../images/side-profile-pic.jpg';
 import Email from '../images/email-icon.png';
 import Resume from '../images/resume-icon.png';
 import LinkedIn from '../images/linkdin-icon.png';
+import GitHub from '../images/github-icon-black.png';
 
 function Contact(props) {
 	return (
@@ -28,6 +29,15 @@ function Contact(props) {
 					<img className='contact-icon' src={LinkedIn} alt='linkedin' />
 					<a target='_blank' href='https://www.linkedin.com/in/treytowers/'>
 						LinkedIn
+					</a>
+				</div>
+				<div>
+					<img className='contact-icon github' src={GitHub} alt='github' />
+					<a
+						className='git-link'
+						target='_blank'
+						href='https://github.com/Treyceratops'>
+						Github
 					</a>
 				</div>
 			</div>
