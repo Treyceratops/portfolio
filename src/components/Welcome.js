56bcd3c638c4397b092c997639cc591f25542662
Treyceratops <treytowers@yahoo.com>
2022-12-19 13:48:37 -0800

    add welcome-page grid

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import ProfilePic from '../images/profile-pic.jpg';
 
 function Welcome(props) {
 	return (
-		<div>
+		<div className='welcome-page'>
 			<style>
 				{'button { color: white; border: solid 3px white; }'}
 				{'button:hover { color: black; background: white; }'}
@@ -12,18 +12,21 @@ function Welcome(props) {
 				{'.contact { display: none; }'}
 				{'.nav-bar { display: none; }'}
 			</style>
-			<h1 className='welcome-msg'>
-				Hello, <br /> Bonjour, <br /> <span className='japan'>こんにちは!</span>
-			</h1>
-			<h1 className='welcome-p'>
-				My name is <span className='first'>T</span>rey. I am a web-developer and
-				software engineer.
-			</h1>
-			<div>
-				<Link to='/works'>
+			<div className='welcome-text'>
+				<h1 className='welcome-msg'>
+					Hello, <br /> Bonjour, <br />{' '}
+					<span className='japan'>こんにちは!</span>
+				</h1>
+				<h1 className='welcome-p'>
+					My name is <span className='first'>T</span>rey. I am a web-developer
+					and software engineer.
+				</h1>
+			</div>
+			<div className='welcome-butt-img'>
+				<Link className='welcome-button' to='/works'>
 					<button className='enter speech'>Check out more of my work!</button>
 				</Link>
-				<img className='profile-pic main' src={ProfilePic} alt='profile-pic' />
+				<img className='welcome-pic main' src={ProfilePic} alt='profile-pic' />
 			</div>
 		</div>
 	);
